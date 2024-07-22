e9cdff39c5c28c243e1cb37b5c623e3e4c3aba41
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-07-22 08:59:23 -0300

    Criação do projeto

---
+++ src/test/java/com/performance_go/CRUD_Farmacia/CrudFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performance_go.CRUD_Farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
